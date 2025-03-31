73eac6158e858de230c27cbfca0fba41d9c89f1b
Deepanshumelkani77 <deepumelkani123@gmail.com>
2025-03-31 10:39:14 +0530

    set all routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,11 @@
 import React from 'react'
 import {Routes,Route} from 'react-router-dom'
 import Home from './pages/Home.jsx'
-
+import Doctor from './pages/Doctor.jsx'
+import About from './pages/About.jsx'
+import MyProfile from './pages/MyProfile.jsx'
+import MyAppointment from './pages/MyAppointment.jsx'
+import Login from './pages/Login.jsx'
 
 const App = () => {
   return (
@@ -9,7 +13,11 @@ const App = () => {
     <div className='mx-4 sm:mx[10%]'>  
 <Routes>
 <Route path='/' element={<Home/>}/>
-
+<Route path='/doctor' element={<Doctor/>}/>
+<Route path='about' element={<About/>}/>
+<Route path='/myprofile' element={<MyProfile/>}/>
+<Route path='/myappointment' element={<MyAppointment/>}/>
+<Route path='/login' element={<Login/>}/>
 </Routes>
     </div>
   )
