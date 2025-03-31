58620fcffce89b8e0a4b33d9314916e58295dcac
Deepanshumelkani77 <deepumelkani123@gmail.com>
2025-03-31 12:28:41 +0530

    navbar completed

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,15 +1,17 @@
 import React from 'react'
+import { useState } from 'react'
 import {assets} from "../assets/assets"
 import { NavLink, useNavigate } from 'react-router-dom'
 
 const Navbar = () => {
     const navigate=useNavigate();
+const [user,setUser]=useState(true)
 
   return (
     <div className='flex item-center justify-between text-sm py-4 mb-5 border-b border-b-gray-400'>
       <img className='w-44 cursor-pointer' src={assets.logo} alt="" />
        {/* this ul hidden for small width but visible for medium and large */}
-      <ul className='hidden md:flex items-start gap-6 font-medium mt-3 '>
+      <ul className='hidden md:flex items-start gap-8 font-medium mt-3 '>
         <NavLink to='/'>
             <li className='py-1'>HOME</li>
             <hr className='border-none outline-none h-0.5 bg-[#5f6FFF] w-3/5 m-auto hidden ' />
@@ -27,10 +29,30 @@ const Navbar = () => {
             <hr className='border-none outline-none h-0.5 bg-[#5f6FFF] w-3/5  m-auto hidden'/>
         </NavLink>
       </ul>
+
       <div className='flex item-center gap-4 cursor-pointer'>
         {/* this ul hidden for small width but visible for medium and large */}
-        <button onClick={()=>{navigate('/login')}} className='bg-[#5f6FFF] text-white px-8 py-3 rounded-full font-light hidden md:block'>Create account</button>
+        {user ?
+        <div className='flex item-center gap-2 cursor-pointer group relative '>
+            <img src={assets.profile_pic} className='w-8 rounded-full' ></img> 
+            <img src={assets.dropdown_icon} className='w-2.5'></img>
+            {/* when we hover group class when it display */}
+           <div className='absolute top-0 right-0 pt-14 text-base font-medium text-gray-600  z-20 hidden group-hover:block'> 
+            <div className='min-w-48 bg-stone-100 rounded flex flex-col gap-4 p-4'>
+            <p onClick={()=>{navigate("/myprofile")}} className='hover:text-black cursor-pointer'>My Profile</p>
+            <p onClick={()=>{navigate("/myappointment")}} className='hover:text-black cursor-pointer'>My Appointment</p>
+            <p onClick={()=>{setUser(false)}} className='hover:text-black cursor-pointer'>Logout</p> 
+            </div>
+            
+            </div>
+            </div> 
+             
+             
+  :    <button onClick={()=>{navigate('/login')}} className='bg-[#5f6FFF] text-white px-8 py-3 rounded-full font-light hidden md:block'>Create account</button>}
+     
       </div>
+
+
     </div>
   )
 }
