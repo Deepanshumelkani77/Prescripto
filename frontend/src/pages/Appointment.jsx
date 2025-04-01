3956ad72aaf870d3d7fd24e5a39ba692129f4f9d
Deepanshumelkani77 <deepumelkani123@gmail.com>
2025-04-01 17:42:39 +0530

    create appointment

--- frontend/src/pages/Appointment.jsx
+++ frontend/src/pages/Appointment.jsx
@@ -1,11 +1,56 @@
-import React from 'react'
+import React, { useContext, useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { AppContext } from "../context/AppContext";
+import { assets } from "../assets/assets";
 
 const Appointment = () => {
+  const { docId } = useParams();
+
+  const { doctors } = useContext(AppContext);
+
+  const [docInfo, setDocInfo] = useState({});
+
+  const fetchDocInfo = async () => {
+    const docInfo = doctors.find((doc) => doc._id === docId);
+    setDocInfo(docInfo);
+    console.log(docInfo);
+  };
+
+  useEffect(() => {
+    fetchDocInfo();
+  }, [doctors, docId]);
+
   return (
     <div>
-      
-    </div>
-  )
-}
 
-export default Appointment
+      {/*------Doctor details----- */}
+      <div className='flex flex-col sm:flex-row gap-4'>
+        <div>
+          <img className='bg-[#5f6FFF] w-full sm:max-w-72 rounded-lg' src={docInfo.image} alt="" />
+        </div>
+
+        <div className='flex-1 border border-gray-400 rounded-lg p-8 py-7 bg-white mx-2 sm:mx-0 mt-[-80px] sm:mt-0'>
+          <p className='flex items-center gap-2 text-2xl font-medium text-gray-900'>
+            {docInfo.name}
+            <img className='w-5' src={assets.verified_icon}></img>
+          </p>
+          <div className='flex items-center gap-2 text-sm mt-1 text-gray-600'>
+            <p>
+              {docInfo.degree} - {docInfo.speciality}
+            </p>
+            <button className='py-0.5 px-2 border text-x5 rounded-full'>{docInfo.experience}</button>
+          </div>
+          <div>
+            <p className='flex items-center gap-1 text-lg font-medium text-gray-900 mt-3'>
+              About <img src={assets.info_icon}></img>
+            </p>
+            <p className='text-m text-gray-500 max-w-[1000px] mt-1'>{docInfo.about}</p>
+          </div>
+          <p className='text-gray-500 font-medium mt-4'>Appointment fee: <span className='text-gray-600'>${docInfo.fees}</span></p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Appointment;
