bdd9a78c18435318df775ddcd99d1178d5e353eb
Deepanshumelkani77 <deepumelkani123@gmail.com>
2025-04-03 09:12:39 +0530

    make function for slots

--- frontend/src/pages/Appointment.jsx
+++ frontend/src/pages/Appointment.jsx
@@ -10,7 +10,7 @@ const Appointment = () => {
 
   const [docInfo, setDocInfo] = useState({});
 
-  const fetchDocInfo = async () => {
+  const fetchDocInfo =  () => {
     const docInfo = doctors.find((doc) => doc._id === docId);
     setDocInfo(docInfo);
     console.log(docInfo);
@@ -21,10 +21,76 @@ const Appointment = () => {
   }, [doctors, docId]);
 
 
+
+ 
+
+
+
 //state variable for days
  const [docSlots,setDocSlots]=useState([]);
  const [slotIndex,setSlotIndex]=useState(0);
  const [slotTime,setSlotTime]=useState('')
+
+
+ const getAvailableSlots=async()=>{
+setDocSlots([])
+//getting current date
+let today=new Date();
+for(let i=0;i<7;++i)
+{
+  //getting date with index
+  let currentDate=new Date(today)
+  currentDate.setDate(today.getDate()+i);
+
+  //setting end time of the date
+  let endTime=new Date()
+  endTime.setDate(today.getDate()+i);
+  endTime.setHours(21,0,0,0);
+
+  //setting hours
+  if(today.getDate()===currentDate.getDate()){
+
+currentDate.setHours(currentDate.getHours()>10?currentDate.getHours()+1:10)
+currentDate.setMinutes(currentDate.getMinutes()>30?30:0)
+  }else{
+    currentDate.setHours(10)
+    currentDate.setMinutes(0)
+  }
+  
+  let timeSlots=[]
+
+  while(currentDate<endTime){
+    let formattedTime=currentDate.toLocaleTimeString([],{hour:'2-digit',minute:'2-digit'})
+    //add slot to array
+    timeSlots.push({
+      datetime:new Date(currentDate),
+      time:formattedTime
+    })
+
+    //Increment current time by 30 minutes
+    currentDate.setMinutes(currentDate.getMinutes()+30)
+  }
+
+  setDocSlots(prev=>([...prev,timeSlots]))
+
+}
+
+ }
+
+ useEffect(() => {
+  if (docInfo && Object.keys(docInfo).length > 0) {
+    getAvailableSlots();
+  }
+}, [docInfo]);
+
+
+useEffect(() => {
+  if (docSlots.length > 0) {
+    console.log(docSlots);
+  }
+}, [docSlots]);
+
+
 
 
 
