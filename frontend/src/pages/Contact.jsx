304cd6f7fc1db1714dc57bb159cd03d63fea35e1
Deepanshumelkani77 <deepumelkani123@gmail.com>
2025-04-03 12:22:57 +0530

    completed contact page

--- frontend/src/pages/Contact.jsx
+++ frontend/src/pages/Contact.jsx
@@ -9,16 +9,16 @@ const Contact = () => {
     <p>CONTACT <span className='text-gray-700 font-semibold'>US</span></p>
 </div>
 
-<div>
+<div className='my-10 flex flex-col justify-center md:flex-row gap-10 mb-28 text-sm'>
 
-<img src={assets.contact_image} alt="" />
-<div>
-    <p>OUR OFFICE</p>
-    <p>54709 Willms Station <br /> Suite 350 , Washington ,USA</p>
-    <p>Number: 7983458418 <br />Email: deepumelkani123@gmail.com</p>
-    <p>Careers at Prescripto</p>
-    <p>Learn more about teams and job openings</p>
-    <button>Explore Jobs</button>
+<img className='w-full md:max-w-[360px]' src={assets.contact_image} alt="" />
+<div className='flex flex-col justify-center items-start gap-6'>
+    <p className='font-semibold text-lg  text-gray-600 '>OUR OFFICE</p>
+    <p  className='text-gray-500 '>54709 Willms Station <br /> Suite 350 , Washington ,USA</p>
+    <p className='text-gray-500 '>Number: 7983458418 <br />Email: deepumelkani123@gmail.com</p>
+    <p className='font-semibold text-lg text-gray-600'>Careers at Prescripto</p>
+    <p className='text-gray-500'>Learn more about teams and job openings</p>
+    <button className='border border-black px-8 py-4 text-sm hover:bg-black hover:text-white transition-all duration-500'>Explore Jobs</button>
 </div>
 
 </div>
