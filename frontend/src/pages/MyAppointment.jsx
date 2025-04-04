7895f2ed3f8c646f8c693446aeb65e25154a16f3
Deepanshumelkani77 <deepumelkani123@gmail.com>
2025-04-04 12:05:08 +0530

    structure of my appointment has ready

--- frontend/src/pages/MyAppointment.jsx
+++ frontend/src/pages/MyAppointment.jsx
@@ -1,9 +1,38 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { AppContext } from '../context/AppContext'
 
 const MyAppointment = () => {
+const {doctors}=useContext(AppContext)
+
   return (
     <div>
-      
+      <p>My Appointment</p>
+      <div>
+        {
+          doctors.slice(0,2).map((item,index)=>(
+            <div key={index}>
+
+<div>
+  <img src={item.image} alt="" />
+</div>
+
+<div>
+  <p>{item.name}</p>
+  <p>{item.speciality}</p>
+  <p>Address{}</p>
+  <p>{item.address.line1}</p>
+  <p>{item.address.line2}</p>
+  <p><span>Date & Time:</span>5, July,2024 | 8:30 PM</p>
+</div>
+<div></div>
+<div>
+  <button >Pay Online</button>
+  <button>Cancel appointment</button>
+</div>
+            </div>
+          ))
+        }
+      </div>
     </div>
   )
 }
