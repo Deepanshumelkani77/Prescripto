cace06e88c5487e3e04805ab9a93c1112735355e
Deepanshumelkani77 <deepumelkani123@gmail.com>
2025-04-04 11:49:02 +0530

    allmost completed

--- frontend/src/pages/MyProfile.jsx
+++ frontend/src/pages/MyProfile.jsx
@@ -37,30 +37,30 @@ const [isEdit,setIsEdit]=useState(false)
 
 <hr className='bg-zinc-400 h-[1px] border-none' />
 <div>
-  <p classname='text-neutral-500 underline mt-3'>CONTACT INFORMATION</p>
+  <p className='text-neutral-500 underline mt-3'>CONTACT INFORMATION</p>
   <div className='grid grid-cols-[1fr_3fr] gap-y-2.5 mt-3 text-neutral-700'>
-    <p>Email id:</p>
-    <p>{userData.email}</p>
-    <p>Phone:</p>
+    <p className='font-medium '>Email id:</p>
+    <p className='text-blue-500'>{userData.email}</p>
+    <p className='font-medium'>Phone:</p>
     {
   isEdit?
-<input 
+<input className='bg-gray-100 max-w-52' 
   type='text' 
   value={userData.phone} 
   onChange={e => setUserData(prev => ({ ...prev, name: e.target.value }))}
 />:
-  <p>{userData.phone}</p>
+  <p className='text-blue-400'>{userData.phone}</p>
 
 }
-<p>Address:</p>
+<p className='font-medium'>Address:</p>
 {
   isEdit?
   <p>
-<input onChange={(e)=> setUserData(prev=>({...prev,address:{...prev.address,line1:e.target.value}}))} value={userData.address.line1} type='text' />
+<input  className='bg-gray-50' onChange={(e)=> setUserData(prev=>({...prev,address:{...prev.address,line1:e.target.value}}))} value={userData.address.line1} type='text' />
     <br />
-    <input onChange={(e)=> setUserData(prev=>({...prev,address:{...prev.address,line1:e.target.value}}))} value={userData.address.line2} type="text" />
+    <input className='bg-gray-50' onChange={(e)=> setUserData(prev=>({...prev,address:{...prev.address,line1:e.target.value}}))} value={userData.address.line2} type="text" />
   </p>:
-  <p>
+  <p className='text-gray-500'>
     {userData.address.line1}
     <br />
     {userData.address.line2}
@@ -71,30 +71,32 @@ const [isEdit,setIsEdit]=useState(false)
 </div>
 
 <div>
-  <p>BASIC INFORMATION</p>
-  <div>
-    <p>Gender:</p>
+  <p className='text-neutral-500 underline mt-3'>BASIC INFORMATION</p>
+  <div className='grid grid-cols-[1fr_3fr] gap-y-2.5 mt-3 text-neutral-700'>
+    <p className='font-medium '>Gender:</p>
     {
-  isEdit?<select onChange={(e)=>setUserData(prev=>({...prev,gender:e.target.value}))} value={userData.gender} >
+  isEdit?<select className='max-w-20 bg-gray-100' onChange={(e)=>setUserData(prev=>({...prev,gender:e.target.value}))} value={userData.gender} >
     <option value="Male"></option>
     <option value="Female"></option>
   </select>:
-  <p>{userData.gender}</p>
+  <p className='text-gray-400'>{userData.gender}</p>
 
 }
-<p>Birthday:</p>
+<p className='font-medium '>Birthday:</p>
 {
   isEdit?
-  <input type='date' onChange={(e)=>setUserData(prev=>({...prev,dob:e.target.value}))} value={userData.dob} />:
-  <p>{userData.dob}</p>
+  <input className='max-w-28 bg-gray-100' type='date' onChange={(e)=>setUserData(prev=>({...prev,dob:e.target.value}))} value={userData.dob} />:
+  <p className='text-gray-400'>{userData.dob}</p>
 }
   </div>
 </div>
 
-<div>
-  isEdit?
-  <button onClick={()=>setIsEdit(true)}>Edit</button>:
-  <button onClick={()=>setIsEdit(false)}>Save information</button>
+<div className='mt-10'>
+  {
+  isEdit 
+  ?<button className='border border-[#5f6FFF] px-8 py-2 rounded-full hover:bg-[#5f6FFF] hover:text-white transition-all ' onClick={()=>setIsEdit(false)}>Save information</button> :
+  <button className='border border-[#5f6FFF] px-8 py-2 rounded-full hover:bg-[#5f6FFF] hover:text-white transition-all' onClick={()=>setIsEdit(true)}>Edit</button>
+}
 </div>
 
     </div>
