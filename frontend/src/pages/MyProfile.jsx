6c41566967d9f5296e4e350f0db076e6a4b978a1
Deepanshumelkani77 <deepumelkani123@gmail.com>
2025-04-04 11:30:12 +0530

    set css property

--- frontend/src/pages/MyProfile.jsx
+++ frontend/src/pages/MyProfile.jsx
@@ -20,25 +20,25 @@ const [userData,setUserData]=useState({
 const [isEdit,setIsEdit]=useState(false)
 
   return (
-    <div>
+    <div className=' max-w-lg flex flex-col gap-2 text-sm '>
       
-<img src={userData.image} alt="" />
+<img className='w-36 rounded' src={userData.image} alt="" />
 
 {
   isEdit?
-<input 
+<input className='bg-gray-50 text-3xl font-medium max-w-60 mt-4' 
   type='text' 
   value={userData.name} 
   onChange={e => setUserData(prev => ({ ...prev, name: e.target.value }))}
 />:
-  <p>{userData.name}</p>
+  <p className='font-medium text-3xl text-neutral-800 mt-4'>{userData.name}</p>
 
 }
 
-<hr />
+<hr className='bg-zinc-400 h-[1px] border-none' />
 <div>
-  <p>CONTACT INFORMATION</p>
-  <div>
+  <p classname='text-neutral-500 underline mt-3'>CONTACT INFORMATION</p>
+  <div className='grid grid-cols-[1fr_3fr] gap-y-2.5 mt-3 text-neutral-700'>
     <p>Email id:</p>
     <p>{userData.email}</p>
     <p>Phone:</p>
