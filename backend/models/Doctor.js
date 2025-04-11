380f339d4a5bd1834bfb9d59f0d8ddf7cbf4ad3c
Deepanshumelkani77 <deepumelkani123@gmail.com>
2025-04-11 08:57:36 +0530

    doctor schema completed

--- backend/models/Doctor.js
+++ backend/models/Doctor.js
@@ -2,6 +2,19 @@ const mongoose = require("mongoose");
 
 const doctorSchema=new mongoose.Schema({
 
+    name:{type:String,required:true},
+image:{type:String,required:true},
+speciality:{type:String,required:true},
+degree:{type:String,required:true},
+experience: {type:String,required:true},
+about:  {type:String,required:true},
+fees: {type:Number,required:true} ,
+address:{
+    line1: {type:Number,required:true} ,
+    line2:   {type:Number,required:true}
+}
+
+
 
 
 
