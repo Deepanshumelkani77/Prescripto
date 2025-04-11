0090b63a7fb3eb9a58cb3128a9b03f2e77eadd2a
Deepanshumelkani77 <deepumelkani123@gmail.com>
2025-04-11 09:07:25 +0530

    completed

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -5,7 +5,7 @@ import Sidebar from './components/Sidebar'
 import AddDcotor from './pages/AddDoctor'
 import AllDoctor from './pages/AllDoctor'
 import { Route, Routes } from 'react-router-dom'
-
+import EditDoctor from './pages/EditDoctor'
 
 
 const App = () => {
@@ -27,6 +27,7 @@ const [showLogin,setShowLogin]=useState(false)
   <Route path='/' element={<AllDoctor/>}></Route>
 
     <Route path='/add-doctor' element={<AddDcotor/>}></Route>
+    <Route path='/edit-doctor' element={<EditDoctor/>}></Route>
    
   </Routes>
 </div>
