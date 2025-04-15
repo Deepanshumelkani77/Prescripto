5fcd3732275c00e27b1fd0c5b001dab69cafba8b
Deepanshumelkani77 <deepumelkani123@gmail.com>
2025-04-15 09:09:31 +0530

    create doctor logib button

--- frontend/src/components/Banner.jsx
+++ frontend/src/components/Banner.jsx
@@ -1,9 +1,11 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { assets } from '../assets/assets'
 import { useNavigate } from 'react-router-dom'
+import { AppContext } from '../context/AppContext'
 
 const Banner = () => {
 const navigate=useNavigate()
+const {state,setState}=useContext(AppContext)
 
   return (
     <div className='flex bg-[#5f6FFF] rounded-lg px-6 sm:px-10 md:px-14 lg:px-12 my-20 md:mx-10'>
@@ -15,7 +17,12 @@ const navigate=useNavigate()
         <p>Book Appointment</p>
         <p className='mt-4'>With 100+ Trusted Doctors</p>
     </div>
+    <div className='flex gap-4'>
     <button onClick={()=>{navigate('/login'); scrollTo(0,0)}} className='bg-white text-sm sm:text-base text-gray-600 px-8 py-3 rounded-full mt-6 hover:scale-105 transition-all'>Create account</button>
+    <button onClick={()=>{setState("Doctor"); scrollTo(0,0)}} className='bg-white text-sm sm:text-base text-gray-600 px-8 py-3 rounded-full mt-6 hover:scale-105 transition-all'>Doctor Login</button>
+
+    </div>
+ 
 </div>
 
 
