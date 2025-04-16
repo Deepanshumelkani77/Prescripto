834ef7fb8766958313d447bb07641e8518595d3b
Deepanshumelkani77 <deepumelkani123@gmail.com>
2025-04-16 12:14:32 +0530

    route file create form  appointment

---
+++ backend/routes/Appointment.js
@@ -0,0 +1,12 @@
+const express=require("express");
+const router=express.Router();
+const Appointment = require("../models/Appointment.js");
+const mongoose = require("mongoose");
+
+
+
+
+
+
+
+module.exports=router
