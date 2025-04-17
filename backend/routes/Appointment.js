80a66d6a64d69ff4ed22196530161e2d36ca9b5e
Deepanshumelkani77 <deepumelkani123@gmail.com>
2025-04-17 09:45:21 +0530

    route for appointment completed

--- backend/routes/Appointment.js
+++ backend/routes/Appointment.js
@@ -4,6 +4,23 @@ const Appointment = require("../models/Appointment.js");
 const mongoose = require("mongoose");
 
 
+router.post("/",async(req, res) => {
+    try{
+  const { user_id,day,date,time} = req.body;
+console.log(req.body)
+const appointment1=new Appointment({user_id:user_id,day:day,date:date,time:time})
+appointment1.save();  
+res.status(201).send({ message: "Appointment successfully done" });
+
+  
+}catch (err) {
+  console.error(err);
+  res.status(500).send({ message: "Something went wrong" });
+}
+
+
+}
+)
 
 
 
