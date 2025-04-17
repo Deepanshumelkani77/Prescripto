d1f4ea957258759734c0a55362c448ddf59d364b
Deepanshumelkani77 <deepumelkani123@gmail.com>
2025-04-17 14:25:24 +0530

    sidebar set successfully

--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -8,11 +8,11 @@ const Sidebar = () => {
   <ul className='text-[#515151] mt-5'>
     <NavLink  className={({isActive})=> `flex itms-center gap-3 py-3.5 px-3 md:px-9 md:min-w-72 cursor pointer ${isActive? 'bg-[#F2F3FF] border-r-4 border-[#5f6FFF]':''} `} to='/'>
       <img src={assets1.people_icon} alt="" />
-      <p>Profile</p>
+      <p className='hidden md:block'>Profile</p>
     </NavLink >
     <NavLink className={({isActive})=> `flex itms-center gap-3 py-3.5 px-3 md:px-9 md:min-w-72 cursor pointer ${isActive? 'bg-[#F2F3FF] border-r-4 border-[#5f6FFF]':''} `} to='/doctor-appointment'>
       <img src={assets1.appointment_icon} alt="" />
-      <p>Appointments</p>
+      <p className='hidden md:block'>Appointments</p>
     </NavLink>
   
   </ul>
