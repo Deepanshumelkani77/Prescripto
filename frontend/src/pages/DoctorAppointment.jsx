092abcc14891e5a1c3bc52de2e522ec88d454201
Deepanshumelkani77 <deepumelkani123@gmail.com>
2025-04-17 14:12:29 +0530

    create 7 paragraph

--- frontend/src/pages/DoctorAppointment.jsx
+++ frontend/src/pages/DoctorAppointment.jsx
@@ -2,8 +2,21 @@ import React from 'react'
 
 const DoctorAppointment = () => {
   return (
-    <div>
+    <div className='w-full max-w-6xl m-5'>
       <p>All Appointment</p>
+
+<div>
+  <div>
+    <p>#</p>
+    <p>patient</p>
+    <p>payment</p>
+    <p>age</p>
+    <p>date & time</p>
+    <p>fees</p>
+    <p>action</p>
+  </div>
+</div>
+
     </div>
   )
 }
