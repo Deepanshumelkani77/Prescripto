7befda1aea23d3511060f21e3e300b7d1c9a70cf
Deepanshumelkani77 <deepumelkani123@gmail.com>
2025-04-17 14:18:19 +0530

    set className for div

--- frontend/src/pages/DoctorAppointment.jsx
+++ frontend/src/pages/DoctorAppointment.jsx
@@ -6,7 +6,7 @@ const DoctorAppointment = () => {
       <p className='mb-3 text-lg font-medium'>All Appointment</p>
 
 <div className='bg-white border rounded text-sm max-h-[80vh] min-h-[50vh] overflow-y-scroll'>
-  <div>
+  <div className='max-sm:hidden grid grid-cols-[0.5fr_2fr_1fr_1fr_3fr_1fr_1fr] gap-1 py-3 px-6 border-b'>
     <p>#</p>
     <p>patient</p>
     <p>payment</p>
